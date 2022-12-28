26efe7a90c92098949499b3add6f90abb6bd6670
SayamBar <sayam.bar@bitcanny.com>
2022-12-28 12:05:33 +0530

    first commit

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,38 @@
+class LikesController < ApplicationController
+    before_action :find 
+    def create
+        if(params[:post_id])
+            @post.likes.create(user_id: current_user.id)
+            redirect_to post_path(@post)
+        else
+            @comment.likes.create(user_id: current_user.id)
+            post = Post.find(@comment.post_id)
+            redirect_to post_path(post)
+        end
+    end
+    def destroy
+        if(params[:post_id])
+            l = Like.where(user_id: current_user.id,likeable_type: "Post",likeable_id: @post.id)
+            Like.destroy(l.ids)
+            redirect_to post_path(@post)
+        else
+            l = Like.where(user_id: current_user.id,likeable_type: "Comment",likeable_id: @comment.id)
+            Like.destroy(l.ids)
+            post = Post.find(@comment.post_id)
+            redirect_to post_path(post)
+        end
+        
+    end
+    
+    private  
+        def find
+            if(params[:post_id])
+                @post = Post.find(params[:post_id])
+            else
+                @comment = Comment.find(params[:comment_id])
+            end
+        end   
+end
+
+
+  
---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :users
+  resources :posts do
+    resources :comments
+  end
+
+    
+  # end
+  resources :posts do
+    resources :likes
+  end
+
+  resources :comments do
+    resources :likes
+  end
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root to: "posts#index"
+end
